1e3728ee962e6fdee1ccd588f29c37efc9a59e2e
duytn <tangocduy13@gmail.com>
2023-12-15 13:42:24 +0700

    update view my booking

--- backend/controllers/bookingDetailController.js
+++ backend/controllers/bookingDetailController.js
@@ -1,20 +1,90 @@
 const BookingDetail = require('../models/BookingDetail');
+const mongoose = require('mongoose')
 
 const getBookingDetailByBookingId = async (req, res) => {
     try {
         const bookingId = req.params.bookingId;
-        const bookingDetails = await BookingDetail.find({ bookingId })?.populate('serviceId').populate('petId');
-        if (!bookingDetails) {
-            return res.status(404).json({ 
-                message: 'BookingDetail not found!' 
-            });
-          }
-        res.status(200).json(bookingDetails)
+
+        const bookingDetails = await BookingDetail.aggregate([
+            {
+                $match: { bookingId: new mongoose.Types.ObjectId(bookingId) }
+            },
+            {
+                $lookup: {
+                    from: 'services',
+                    localField: 'serviceId',
+                    foreignField: '_id',
+                    as: 'service'
+                }
+            },
+            {
+                $unwind: "$service"
+            },
+            {
+                $lookup: {
+                    from: 'pets',
+                    localField: 'petId',
+                    foreignField: '_id',
+                    as: 'pet'
+                }
+            },
+            {
+                $unwind: "$pet"
+            },
+            {
+                $addFields: {
+                    isWithinSale: {
+                        $and: [
+                            { $gte: ["$createdAt", "$service.saleStartTime"] },
+                            { $lte: ["$createdAt", "$service.saleEndTime"] }
+                        ]
+                    },
+                    discountedPrice: {
+                        $cond: {
+                            if: {
+                                $and: [
+                                    "$service.saleStartTime",
+                                    "$service.saleEndTime",
+                                    { $gte: ["$createdAt", "$service.saleStartTime"] },
+                                    { $lte: ["$createdAt", "$service.saleEndTime"] }
+                                ]
+                            },
+                            then: {
+                                $multiply: [
+                                    "$quantity",
+                                    { $subtract: ["$service.price", { $multiply: ["$service.price", { $divide: ["$service.discount", 100] }] }] }
+                                ]
+                            },
+                            else: { $multiply: ["$quantity", "$service.price"] }
+                        }
+                    }
+                }
+            },
+            {
+                $project: {
+                    bookingId: 1,
+                    petId: 1,
+                    serviceId: 1,
+                    quantity: 1,
+                    price: "$discountedPrice",
+                    service: 1,
+                    pet: 1,
+                    createdAt: 1,
+                    updatedAt: 1
+                }
+            }
+        ]);
+
+        if (!bookingDetails || bookingDetails.length === 0) {
+            return res.status(404).json({ message: 'BookingDetail not found!' });
+        }
+
+        res.status(200).json(bookingDetails);
     } catch (err) {
-        console.log(err)
-        res.status(500).json(err)
+        console.log(err);
+        res.status(500).json(err);
     }
-}
+};
 
 const createBookingDetail = async (req, res) => {
     try {
@@ -22,7 +92,7 @@ const createBookingDetail = async (req, res) => {
         const bookingId = req.params.bookingId;
         const newBookingDetail = new BookingDetail({ bookingId, serviceId, quantity });
         const result = await newBookingDetail.save();
-        if (!result){
+        if (!result) {
             return res.status(404).json({
                 error: "Can not create BookingDetail"
             })
--- backend/models/BookingDetail.js
+++ backend/models/BookingDetail.js
@@ -1,24 +1,29 @@
 const mongoose = require('mongoose')
 
-const bookingDetailSchema = new mongoose.Schema({
-    bookingId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Booking",
-        required: true,
+const bookingDetailSchema = new mongoose.Schema(
+    {
+        bookingId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Booking",
+            required: true,
+        },
+        petId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Pet",
+            required: true,
+        },
+        serviceId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Service",
+        },
+        quantity: {
+            type: Number,
+            default: 1,
+        }
     },
-    petId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Pet",
-        required: true,
-    },
-    serviceId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Service",
-    },
-    quantity: {
-        type: Number,
-        default: 1,
+    {
+        timestamps: true,
     }
-})
+)
 
 module.exports = mongoose.model("BookingDetail", bookingDetailSchema)
