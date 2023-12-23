bd92087aeba1fd6308f93fc9bd0c3725d1623614
duytn <tangocduy13@gmail.com>
2023-12-23 18:00:20 +0700

    Update usersController.js

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -118,10 +118,6 @@ const updateUser = async (req, res) => {
         user.status = status
         user.userImage = userImage
 
-        // check duplicate phone number
-        // const userFindByEmail = await User.findOne({ email })
-        // const userFindByPhone = await User.findOne({ phone })
-
         const [userFindByEmail, userFindByPhone] = await Promise.all([
             User.findOne({ email }),
             User.findOne({ phone })
