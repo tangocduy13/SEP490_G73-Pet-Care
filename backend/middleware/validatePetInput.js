d6a739c187ce9e93db57d2559b723c9f9b5ff234
duytn <tangocduy13@gmail.com>
2023-12-24 15:12:34 +0700

    update validate
    - height and weight for pet

--- backend/middleware/validatePetInput.js
+++ backend/middleware/validatePetInput.js
@@ -1,10 +1,37 @@
 const { body, validationResult } = require('express-validator')
+const validator = require('validator')
 
 const validateCreatePet = [
     body('userId').trim().notEmpty().withMessage('Không nhận được ID người dùng'),
     body('petName').trim().notEmpty().withMessage('Vui lòng nhập tên thú cưng')
         .matches(/^[\p{L}\s]+$/u).withMessage('Tên sản phẩm không chứa ký tự đặc biệt'),
     body('categoryId').trim().notEmpty().withMessage('Vui lòng chọn loại thú cưng'),
+    body('height')
+        .trim()
+        .optional({ nullable: true })
+        .custom((value, { req }) => {
+            if (value === "") return true;
+            if (!validator.default.isNumeric(value)) {
+                throw new Error("Chiều cao phải là một số")
+            }
+            if (parseFloat(value) <= 0) {
+                throw new Error("Chiều cao phải lớn hơn 0")
+            }
+
+        }),
+    body('weight')
+        .trim()
+        .optional({ nullable: true })
+        .custom((value, { req }) => {
+            if (value === "") return true;
+            if (!validator.default.isNumeric(value)) {
+                throw new Error("Cân nặng phải là một số")
+            }
+            if (parseFloat(value) <= 0) {
+                throw new Error("Cân nặng phải lớn hơn 0")
+            }
+
+        }),
     body('color').optional().isString().withMessage('Màu lông không hợp lệ'),
 
     (req, res, next) => {
@@ -21,6 +48,32 @@ const validateUpdatePet = [
     body('petName').trim().notEmpty().withMessage('Vui lòng nhập tên thú cưng')
         .matches(/^[\p{L}\s]+$/u).withMessage('Tên thú cưng không chứa ký tự đặc biệt'),
     body('categoryId').trim().notEmpty().withMessage('Vui lòng chọn loại thú cưng'),
+    body('height')
+        .trim()
+        .optional({ nullable: true })
+        .custom((value, { req }) => {
+            if (value === "") return true;
+            if (!validator.default.isNumeric(value)) {
+                throw new Error("Chiều cao phải là một số")
+            }
+            if (parseFloat(value) <= 0) {
+                throw new Error("Chiều cao phải lớn hơn 0")
+            }
+
+        }),
+    body('weight')
+        .trim()
+        .optional({ nullable: true })
+        .custom((value, { req }) => {
+            if (value === "") return true;
+            if (!validator.default.isNumeric(value)) {
+                throw new Error("Cân nặng phải là một số")
+            }
+            if (parseFloat(value) <= 0) {
+                throw new Error("Cân nặng phải lớn hơn 0")
+            }
+
+        }),
     body('color').optional().isString().withMessage('Màu lông không hợp lệ'),
 
     (req, res, next) => {
